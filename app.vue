77c1f9e7796fca94676f46b5399dfcb9bde25487
P-a Holliger <pa.holliger@gmail.com>
2024-09-12 01:19:16 +0200

    Start style (start)

--- app.vue
+++ app.vue
@@ -1,21 +1,39 @@
 <template>
-  <div class="flex items-center justify-center w-full h-screen bg-neutral-100">
-    <div class="bg-white rounded-lg p-4 shadow max-w-[740px] flex flex-col gap-4 overflow-hidden">
+  <div class="flex flex-col items-center justify-center w-full h-screen text-base bg-stone-800">
+    <div class="flex items-center justify-between w-full gap-2 p-4">
       <div class="flex items-center gap-2">
-        <NuxtLink
-          v-if="!isHome"
-          class="leading-none"
-          to="/"
+        <img
+          src="~/assets/images/LogoLight.svg"
+          alt="Journey UI"
+          class="size-6"
         >
-          <i class="text-lg transition-colors duration-150 i-mdi-arrow-left hover:text-rose-500" />
-        </NuxtLink>
-
-        <strong>
-          DS Journey
-        </strong>
+        <span class="text-lg font-bold text-stone-100">
+          JourneyUI
+        </span>
       </div>
 
-      <NuxtPage />
+      <span class="transition-opacity opacity-50 select-none text-stone-100 hover:opacity-100">
+        Work in progress, be kind! 🌼
+      </span>
+    </div>
+    <div class="flex items-center justify-center flex-1">
+      <div class="bg-white rounded-lg p-4 shadow max-w-[740px] flex flex-col gap-4 overflow-hidden">
+        <div class="flex items-center gap-2">
+          <NuxtLink
+            v-if="!isHome"
+            class="leading-none"
+            to="/"
+          >
+            <i class="text-lg transition-colors duration-150 i-mdi-arrow-left hover:text-rose-500" />
+          </NuxtLink>
+
+          <strong>
+            DS Journey
+          </strong>
+        </div>
+
+        <NuxtPage />
+      </div>
     </div>
   </div>
 </template>
